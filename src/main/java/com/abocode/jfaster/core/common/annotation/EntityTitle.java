a9a98ded05f010fd8de375a478765fbc9db6bcbb
guan xf <guanxf@aliyun.com>
2023-12-12 14:27:40 +0800

    合并代码

--- src/main/java/com/abocode/jfaster/core/common/annotation/EntityTitle.java
+++
@@ -1,8 +0,0 @@
-package com.abocode.jfaster.core.common.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-@Retention(RetentionPolicy.RUNTIME)
-public @interface EntityTitle {
-	  String name();
-}
--- src/main/java/com/abocode/jfaster/core/extend/datasource/DataSourceType.java
+++
@@ -1,5 +0,0 @@
-package com.abocode.jfaster.core.extend.datasource;
-
-public enum DataSourceType {
-	dataSource_jeecg,dataSource_enter,dataSource4,mapdataSource
-}
